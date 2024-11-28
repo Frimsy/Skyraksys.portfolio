3dd80477668fe7e861fc0a0a66a31056d09e8ed0
Frimsy <frimsyfristole@gmail.com>
2024-11-28 07:53:52 +0530

    Initial commit for SkyRakSys portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// Lazy Load Images
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('img');
+
+    images.forEach((img) => {
+        img.setAttribute('loading', 'lazy');
+    });
+});
